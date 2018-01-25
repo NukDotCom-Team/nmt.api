7e1fd27823652d53d21198207b7065f789a9911e
NukDokPlex <vik.titoff2014@yandex.ru>
2018-01-26 00:14:23 +0500

    Added get_news.php with bug
    This bug make invalid JSON code, based on quotes issue. Please wait or make a fork with bugfix.

--- get_news.php
+++ get_news.php
@@ -7,4 +7,42 @@ function replace_substr($str, $what, $with){
     }
     return $str;
 }
-$output = file_get_contents("http://www.nmt.edu.ru/");
+function escapeJsonString($value) {
+    # list from www.json.org: (\b backspace, \f formfeed)
+    $escapers =     array("\r");
+    $replacements = array("");
+    $result = str_replace($escapers, $replacements, $value);
+    return $result;
+}
+$json=array();
+$limit = $_REQUEST['limit'];
+$start = $_REQUEST['start'];
+$output = file_get_contents("http://www.nmt.edu.ru/index.php?limit=".$limit."&start=".$start);
+$html = str_get_html($output);
+$name = $html->find("div.componentheading", 0);
+$name = $name->innertext();
+$json['status'] = "200";
+$json['name'] = $name;
+$news = $html->find("div.itemList",0);
+$news = str_get_html($news->innertext());
+$items = $news->find("div.itemContainer");
+$itemsArray = array();
+$html->clear();
+$i = 0;
+foreach ($items as $item){
+    $itemCode = str_get_html($item->innertext());
+    $header = $itemCode->find("h3.catItemTitle",0);
+    $title = replace_substr($header->plaintext,'"', "");
+    $itemArray = [
+        'title' => $title,
+        'link' => $itemCode->find("a.k2ReadMore", 0)->href,
+        'body' => $itemCode->find("div.catItemIntroText",0)->innertext()
+    ];
+    echo $i+1 . ". ".$title."<br>";
+    $itemsArray[$i] = $itemArray;
+    $i++;
+}
+$json['items'] = $itemsArray;
+$news->clear();
+echo replace_substr(json_encode($json, JSON_UNESCAPED_UNICODE), "\\t", "");
+//This Code is not working. Please wait until it will be continued.
