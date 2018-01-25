a186b21828d95fdae54c5aed0f3f861963117b64
NukDokPlex <vik.titoff2014@yandex.ru>
2018-01-25 21:56:17 +0500

    Deleted useless strokes of code

--- get_schedule.php
+++ get_schedule.php
@@ -6,14 +6,9 @@ function replace_substr($str, $what, $with){
     }
     return $str;
 }
-$noway = [
-    "&nbsp", "&nbnbsp"
-];
-
 require "simple_html_dom.php";
 $days = $_REQUEST['days'];
 $output = file_get_contents("http://www.nmt.edu.ru/index.php?option=com_schedule&view=schedule&days=".$days);
-//$output = file_get_contents("test.html");
 $html = str_get_html($output);
 $schedule_name = $html->find('div[style=\'font-size: 18px; padding-bootom: 5px;\']', 0)->innertext();
 $change_name = $html->find('div[style=\'font-size: 18px; padding-bootom: 5px;\']', 1)->innertext();
@@ -24,18 +19,13 @@ $paras = $schedule->find('th.schedule, th.end_cell');
 $auditories_work = $schedule->find('td.first_cell');
 $auditories = array();
 $i = 0;
-
-
 foreach ($auditories_work as $auditory){
     $auditories[$i] = $auditory->innertext();
     $i++;
 }
-
 $sessions_work = $schedule->find('td.schedule');
 $i = 0;
 $sessions = array();
-
-
 foreach ($sessions_work as $session){
     $session_processed = replace_substr($session->innertext(), "&nbsp;", "");
     $session_processed = replace_substr($session_processed, "&nbnbsp;", "");
@@ -43,7 +33,6 @@ foreach ($sessions_work as $session){
     $sessions[$i] = $session_processed;
     $i++;
 }
-
 $json = array();
 $json['status'] = "200";
 $json['days'] = $days;
@@ -52,4 +41,3 @@ $json['change_name'] = $change_name;
 $json['auditories'] = $auditories;
 $json['sessions'] = $sessions;
 echo json_encode($json, JSON_UNESCAPED_UNICODE);
-//echo "Elapsed time is: ". round(microtime(true) - $startTime, 4) ." seconds";
